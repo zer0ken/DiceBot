5c3213c0692cfba56fa4584591af12ac92a3e7ec
zer0ken <31297454+zer0ken@users.noreply.github.com>
2019-08-09 19:32:44 +0900

    ...

--- trpg_bot.py
+++ trpg_bot.py
@@ -5,7 +5,7 @@ from os import environ
 from discord import Embed, Colour, Message, Member, TextChannel, VoiceChannel
 from discord.ext import commands
 
-command_prefix = '..'
+command_prefix = '>'
 
 
 class TRPGCog(commands.Cog):
@@ -13,25 +13,25 @@ class TRPGCog(commands.Cog):
     
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
-        self.private_rooms: dict = {}
+        
+    @commands.command(name='초대링크', brief='봇을 초대하기 위한 링크를 확인합니다.')
+    async def invite_link(self, ctx: commands.Context):
+        url = 'https://discordapp.com/api/oauth2/authorize?client_id=609223331945906186&permissions=16788560&scope=bot'
+        embed = Embed(title=':link: 봇 초대 링크', colour=Colour.blurple(),
+                      description=f'봇을 다른 서버에 초대하려면 **[여기]({url})**를 클릭하세요.')
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        await ctx.send(ctx.author.mention, embed=embed)
     
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('ready.')
-    
-    @commands.Cog.listener()
-    async def on_message(self, msg: Message):
-        print(msg.author, msg.content)
-    
-    @commands.command(name='도움말', aliases=('도움', 'h', 'help'), brief=f'명령어의 도움말을 확인합니다.',
+    @commands.command(name='도움말', aliases=('도움', 'h', 'help'), brief='명령어의 도움말을 확인합니다.',
                       help='명령어의 도움말을 확인합니다.\n'
                            '사용법은 다음과 같습니다.\n'
                            '```\n'
-                      f'{command_prefix}도움말 굴림\n'
-                      f'{command_prefix}도움말 명령어\n'
-                      f'{command_prefix}도움말 비밀방\n'
+                           f'{command_prefix}도움말 굴림\n'
+                           f'{command_prefix}도움말 명령어\n'
+                           f'{command_prefix}도움말 비밀방\n'
                            '```\n'
-                      f'봇이 어떤 기능을 제공하는지 알고 싶다면 "{command_prefix}명령어" 명령어를 사용해보세요.')
+                           f'봇이 어떤 기능을 제공하는지 알고 싶다면 "{command_prefix}명령어" 명령어를 사용해보세요.')
     async def h(self, ctx: commands.Context, *tokens: str):
         cmd: commands.Bot = self.bot
         if tokens:
@@ -94,12 +94,15 @@ class TRPGCog(commands.Cog):
                       help='주사위를 굴립니다.\n'
                            '사용법은 다음과 같습니다.\n'
                            '```\n'
-                      f'{command_prefix}굴림 D%\n'
-                      f'{command_prefix}굴림 10D + 5\n'
-                      f'{command_prefix}굴림 10 + 2D20 + 4 + 11 + 4D\n'
-                      f'```\n'
-                      f'굴림 기능은 덧셈을 지원하지만 뺄셈은 지원하지 않습니다.\n')
-    async def r(self, ctx: commands.Context, *, roll: str):
+                           '[육면체 주사위 굴림]\n'
+                           f'{command_prefix}굴림\n\n'
+                           '[복잡한 주사위 굴림]\n'
+                           f'{command_prefix}굴림 D%\n'
+                           f'{command_prefix}굴림 10D + 5\n'
+                           f'{command_prefix}굴림 10 + 2D20 + 4 + 11 + 4D\n'
+                           '```\n'
+                           '굴림 기능은 덧셈을 계산하지만 뺄셈은 계산하지 않습니다.\n')
+    async def r(self, ctx: commands.Context, *, roll: str = 'D'):
         print('detected')
         parts = []
         if '+' in roll:
@@ -149,12 +152,19 @@ class TRPGCog(commands.Cog):
         embed.set_thumbnail(url=self.bot.user.avatar_url)
         await ctx.send(ctx.author.mention, embed=embed)
     
-    @commands.group(name='개인방', aliases=('pc', 'pr', 'private', '비밀방'), invoke_without_command=True,
-                    brief='개인방을 만듭니다.',
-                    help='특정 멤버만 볼 수 있는 텍스트/음성 채널을 만듭니다.\n'
-                         '사용법은 다음과 같습니다.\n'
-                         '[멤버]')
-    async def private(self, ctx: commands.Context, *members: Member):
+    @commands.command(name='개인방', aliases=('pc', 'pr', 'private', '비밀방'),
+                      brief='개인방을 만듭니다.',
+                      help='특정 멤버만 볼 수 있는 텍스트/음성 채널을 만듭니다.\n'
+                           '사용법은 다음과 같습니다.\n'
+                           '```\n'
+                           f'{command_prefix}개인방 @userName#1234\n'
+                           f'{command_prefix}개인방 userName\n'
+                           f'{command_prefix}개인방 @secondUser#5678 @andThird#9012 @fourth#3456\n'
+                           f'{command_prefix}개인방 secondUser andThird fourth'
+                           '```\n'
+                           '`@userName#1234` 형식으로 참여하는 멤버를 언급할 수 없다면 '
+                           '`userName` 형식으로 이름만 입력해도 명령어가 제대로 작동합니다.')
+    async def private_room(self, ctx: commands.Context, *members: Member):
         msg = await ctx.send(f'{ctx.author.mention} 개인방을 만들고 있습니다...')
         text_channel: TextChannel = await ctx.guild.create_text_channel(f'개인방 {self.object_id}호실')
         voice_channel: VoiceChannel = await ctx.guild.create_voice_channel(f'개인방 {self.object_id}호실')
@@ -166,11 +176,45 @@ class TRPGCog(commands.Cog):
             tasks.extend([text_channel.set_permissions(member, read_messages=True),
                           voice_channel.set_permissions(member, read_messages=True)])
         await wait(tasks)
-        await msg.edit(content=f'{ctx.author.mention} 개인방을 만들었습니다.')
-        if ctx.guild.id not in self.private_rooms:
-            self.private_rooms[ctx.guild.id] = {}
-        self.private_rooms[ctx.guild.id][self.object_id] = {'text': text_channel.id, 'voice': voice_channel.id}
+        text_url = f'https://discordapp.com/channels/{ctx.guild.id}/{text_channel.id}'
+        voice_url = f'https://discordapp.com/channels/{ctx.guild.id}/{voice_channel.id}'
+        embed = Embed(title=':spy: 개인방 개설', colour=Colour.blurple(), description='개인방을 개설했습니다.')
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        embed.add_field(name=':busts_in_silhouette: 멤버', inline=False,
+                        value=f':small_orange_diamond: {ctx.author}\n' +
+                              ('\n:small_blue_diamond: '.join(str(member) for member in members)))
+        embed.add_field(name=':envelope: 텍스트 채널', value=f'텍스트 채널을 보려면 **[여기]({text_url})**를 클릭하세요.')
+        embed.add_field(name=':loud_sound: 음성 채널 (화면공유)',
+                        value=f'음성 채널에서 화면공유를 활성화하려면 **[여기]({voice_url})**를 클릭하세요.')
+        await msg.edit(content=f'{ctx.author.mention} 개인방을 만들었습니다.', embed=embed)
+        await text_channel.send(embed=embed)
         self.object_id += 1
+        
+    @commands.command(name='화면공유', aliases=('화공', 'ㅎ', 'g', 'G', '화면', '공유', 'screen'),
+                      brief='화면공유 링크를 확인합니다.',
+                      help='음성 채널의 화면공유를 활성화하는 링크를 확인합니다.\n'
+                           '사용법은 다음과 같습니다.\n'
+                           '```\n'
+                           '[현재 연결된 음성 채널에서 화면공유]\n'
+                           f'{command_prefix}화면공유\n\n'
+                           '[특정 음성 채널에서 화면공유]\n'
+                           f'{command_prefix}화면공유 voice1'
+                           f'{command_prefix}화면공유 559653353890643972\n'
+                           '```\n'
+                           '음성 채널을 지정할 때에는 음성 채널의 이름이나 ID를 명령어 뒤에 입력하면 됩니다.')
+    async def screen_share(self, ctx: commands.Context, *, voice_channel: VoiceChannel = None):
+        if voice_channel is None and ctx.author.voice is not None and ctx.author.voice.channel is not None:
+            voice_channel = ctx.author.voice.channel
+        if voice_channel is None:
+            await ctx.send(f'{ctx.author.mention} 화면공유를 활성화할 채널을 지정해주세요.')
+            return
+        url = f'https://discordapp.com/channels/{ctx.guild.id}/{voice_channel.id}'
+        embed = Embed(title=f':desktop: **{ctx.guild}/{voice_channel}**', colour=Colour.blurple(),
+                      description=f'화면공유를 활성화하려면 **[여기]({url})**를 클릭하세요.')
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        await ctx.send(ctx.author.mention, embed=embed)
 
 
 bot = commands.Bot(command_prefix=command_prefix, help_command=None, case_insensitive=True)
